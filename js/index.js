7e1b8a55c336708447632ec815d582deeb3578db
Andre Grech <silver.grech@gmail.com>
2020-10-19 12:57:18 +1100

    modularise promptUser

--- js/index.js
+++ js/index.js
@@ -1,26 +1,9 @@
 const fs = require("fs").promises;
 // fs exported as promises for now
 const util = require("util");
-const readFileAsync = util.promisify(fs.readFile);
+const promptUser = require("promptUser")
 const writeToFileAsync = util.promisify(fs.writeFile);
-const { prompt } = require("inquirer");
-// const
 
-const promptUser = async () => {
-    // read the json file
-    try{
-    const questionsJSON = await readFileAsync("questions.json", "utf-8")
-    const questions = JSON.parse(questionsJSON)
-
-    if (questions) {
-        return await prompt(questions)
-            .then((answers) => answers)
-    }
-    } catch (error) {
-        console.error("Error accessing question or answer data:", error)
-    }
-
-}
 
 // function to initialize program
 init = async () => {
---
+++ js/promptUser.js
@@ -0,0 +1,22 @@
+const { prompt } = require("inquirer");
+const readFileAsync = util.promisify(fs.readFile);
+
+
+
+const promptUser = async () => {
+    // read the json file
+    try{
+    const questionsJSON = await readFileAsync("questions.json", "utf-8");
+    const questions = JSON.parse(questionsJSON);
+
+    if (questions) {
+        return await prompt(questions)
+            .then((answers) => answers);
+    }
+    } catch (error) {
+        console.error("Error accessing question or answer data:", error);
+    }
+
+}
+
+module.exports = promptUser
