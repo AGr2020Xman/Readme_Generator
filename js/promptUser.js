3996b32b5a4775c37f5945755398f58b71811b1e
Andre Grech <silver.grech@gmail.com>
2020-10-19 15:56:53 +1100

    prompt user seems to work - it is prompting. Formatting questions.json fixed.

--- js/promptUser.js
+++ js/promptUser.js
@@ -8,20 +8,19 @@ const readFileAsync = util.promisify(fs.readFile);
 const promptUser = async () => {
     // read the json file
     try{
-    const questionsJSON = await readFileAsync("questions.json", "utf-8").then((data)=>data);
-    console.log(questionsJSON)
+    const questionsJSON = await readFileAsync("questions.json", "utf-8");
     const questions = JSON.parse(questionsJSON);
 
     if (questions) {
         return await prompt(questions)
             .then((answers) => answers);
     }
+
     } catch (error) {
         console.error("Error accessing question or answer data:", error);
     }
 
 }
 
-promptUser()
 
 module.exports = promptUser
