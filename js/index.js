6478656bace7132bf8fa6253d5fb401a5d24adee
Andre Grech <silver.grech@gmail.com>
2020-10-19 12:32:07 +1100

    creating modules to be exported initially in index.js, for prompt user and readFileAsync

--- js/index.js
+++ js/index.js
@@ -1,4 +1,41 @@
-const questions = require("./questions.json")
+const fs = require("fs").promises;
+// fs exported as promises for now
+const util = require("util");
+const { prompt } = require("inquirer");
+// const
+
+const promptUser = async () => {
+    // read the json file
+    try{
+    const questionsJSON = await readFileAsync()
+    const questions = JSON.parse(questionsJSON)
+
+    if (questions) {
+        return await prompt(questions)
+            .then((answers) => answers)
+    }
+    } catch (error) {
+        console.error("Error accessing question or answer data:", error)
+    }
+
+}
+
+
+const readFileAsync = () => 
+    new Promise ((resolve, reject) => {
+        fs.readFile("questions.json", "utf-8", (error, data) => {
+
+            if (error) {
+                reject(console.error("Error reading questions: ", error));
+            }
+          
+           resolve(questionSet);
+          
+          });
+          
+})
+
+
 
 
 // function to write README file
@@ -8,8 +45,10 @@ writeToFile = (fileName, data) => {
 // function to initialize program
 init = async () => {
     try {
+
         const input = await promptUser();
         const generatedReadme = await writeToFile(input);
+        await createFile(generatedReadme)
     } catch (error) {
         if (error) {
             console.log('There was an error, please try again')
