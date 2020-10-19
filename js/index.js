4a27f512b62ffd852b097b18ba6dcf470dd89473
Andre Grech <silver.grech@gmail.com>
2020-10-19 16:22:08 +1100

    added write file pathing

--- js/index.js
+++ js/index.js
@@ -1,19 +1,20 @@
-const fs = require("fs").promises;
+const fs = require("fs");
 // fs exported as promises for now
 const util = require("util");
-const promptUser = require("promptUser")
+const promptUser = require("./promptUser");
+const createFile = require("./filegenerate");
 const writeToFileAsync = util.promisify(fs.writeFile);
 
 
 // function to initialize program
 init = async () => {
     try {
-        const input = await promptUser();
-        const generatedReadme = await writeToFileAsync(input);
-        await createFile(generatedReadme)
-    } catch (error) {
-        if (error) {
-            console.log('There was an error, please try again')
+        const answers = await promptUser();
+        const generatedReadme = createFile(answers);
+        await writeToFileAsync('demo_README.md', generatedReadme, 'utf8');
+    } catch (err) {
+        if (err) {
+            console.error('There was an error, please try again', err)
         }
     }
     console.log("Successfully created readme file!")
